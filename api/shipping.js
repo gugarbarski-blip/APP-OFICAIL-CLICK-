a81b80b6e7b9e4dd3fc7bd5a6f97361bf3adfb85
gugarbarski-blip <gugarbarski@github.com>
2026-05-21 19:48:58 -0300

    fix: use module.exports instead of export default for CJS compat in shipping.js

--- api/shipping.js
+++ api/shipping.js
@@ -153,7 +153,7 @@ async function getUFFromViaCep(cep) {
   }
 }
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).json({ error: 'Method not allowed' });
 
   try {
